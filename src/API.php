ef4f9db3e6c8587e55e6399d71515bfc181e8061
Fèlix Galindo Allué <felix@flx.cat>
2019-01-30 10:17:05 +0100

    dimecres, 30 de gener de 2019, 10:17:05 CET

--- src/API.php
+++ src/API.php
@@ -1,5 +1,8 @@
 <?php
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 class API {
   public function getHello(Request $request, Response $response, array $args) {
     $name = $args['name'];
--- src/main.php
+++ src/main.php
@@ -6,9 +6,6 @@ error_reporting(E_ALL | E_STRICT);
 # Class autoloader
 require '../vendor/autoload.php';
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
 $api = new API();
 $slim = new \Slim\App();
 $slim->get('/hello/{name}', array($api, 'getHello'));
