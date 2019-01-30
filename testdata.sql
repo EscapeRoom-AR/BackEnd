1672835ea5560adb608842b69ef22ce4888ac8bd
Fèlix Galindo Allué <felix@flx.cat>
2019-01-30 11:58:05 +0100

    dimecres, 30 de gener de 2019, 11:58:05 CET

--- testdata.sql
+++ testdata.sql
@@ -1,4 +1,6 @@
-INSERT INTO teachers VALUES (1, 'Fèlix Galindo Allué', 'felix.galindo@stucom.com');
+DELETE FROM teacher;
+INSERT INTO teacher VALUES (1, 'Fèlix Galindo Allué', 'felix.galindo@stucom.com');
 
+DELETE FROM assignment;
 INSERT INTO assignment VALUES (1, 'DAM2T1-M08 PMM (Android)', 1, '2018-09-14', 99);
 INSERT INTO assignment VALUES (2, 'DAM2T2-M08 PMM (Android)', 1, '2018-09-14', 99);
