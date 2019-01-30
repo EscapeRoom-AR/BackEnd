570e404c7066eae6c21c111f72a3d4cd4a452c1b
Fèlix Galindo Allué <felix@flx.cat>
2019-01-30 12:10:48 +0100

    dimecres, 30 de gener de 2019, 12:10:48 CET

--- testdata.sql
+++ testdata.sql
@@ -1,11 +1,12 @@
+DELETE FROM assignment;
 DELETE FROM teacher;
+
 INSERT INTO teacher VALUES (1, 'Fèlix Galindo Allué', 'felix.galindo@stucom.com');
 INSERT INTO teacher VALUES (2, 'Mar Fontana Torroba', 'mar.fontana@stucom.com');
 INSERT INTO teacher VALUES (3, 'Daniel Moreno Arellano', 'daniel.moreno@stucom.com');
 INSERT INTO teacher VALUES (4, 'Cristian Catalán Torán', 'cristian.catalan@stucom.com');
 INSERT INTO teacher VALUES (5, 'Alex Castel', 'alex.castel@stucom.com');
 
-DELETE FROM assignment;
 INSERT INTO assignment VALUES (1, 'DAM2T1-M03 PRO (Programació)', 2, '2018-09-14', 99);
 INSERT INTO assignment VALUES (2, 'DAM2T1-M05 EDE (Entorns de Desenvolupament)', 5, '2018-09-14', 99);
 INSERT INTO assignment VALUES (3, 'DAM2T1-M06 ADA (Accés a Dades)', 2, '2018-09-14', 99);
@@ -14,3 +15,4 @@ INSERT INTO assignment VALUES (5, 'DAM2T1-M08 PMM (Programació Multimèdia i Di
 INSERT INTO assignment VALUES (6, 'DAM2T1-M09 PSP (Programació de serveis i Processos)', 3, '2018-09-14', 99);
 INSERT INTO assignment VALUES (7, 'DAM2T1-M10 SGE (Sistemes de Gestió Empresarial)', 4, '2018-09-14', 99);
 INSERT INTO assignment VALUES (8, 'DAM2T1-M13 PRJ (Projecte)', 3, '2018-09-14', 99);
+INSERT INTO assignment VALUES (9, 'DAM2T1-M14 FCT (Formació en Centres de Treball)', NULL, '2018-09-14', 99);
