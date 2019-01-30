9d3b1a00734a225add51790cd83eebdc1ab19449
Fèlix Galindo Allué <felix@flx.cat>
2019-01-30 10:54:19 +0100

    dimecres, 30 de gener de 2019, 10:54:19 CET

--- src/main.php
+++ src/main.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL | E_STRICT);
 # Class autoloader
 require '../vendor/autoload.php';
 
-$api = new API();
+$api = new \API\API();
 $slim = new \Slim\App();
 $slim->get('/hello/{name}', array($api, 'getHello'));
 $slim->run();
