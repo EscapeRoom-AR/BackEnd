fff0cec72f194e8151dbdf7570083f40f5323eb9
scape.room <scape.room.ar@gmail.com>
2019-03-18 19:33:35 +0100

    Mon 18 Mar 19:33:35 CET 2019

--- dam2t02.db.sql
+++ dam2t02.db.sql
@@ -14,6 +14,27 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `propel_migration`
+--
+
+DROP TABLE IF EXISTS `propel_migration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `propel_migration` (
+  `version` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `propel_migration`
+--
+
+LOCK TABLES `propel_migration` WRITE;
+/*!40000 ALTER TABLE `propel_migration` DISABLE KEYS */;
+/*!40000 ALTER TABLE `propel_migration` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
