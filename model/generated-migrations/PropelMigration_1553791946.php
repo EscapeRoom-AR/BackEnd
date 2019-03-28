80021117462e52c7add24fbe4830c201377f3cf7
scape.room <scape.room.ar@gmail.com>
2019-03-28 17:52:26 +0100

    Thu 28 Mar 17:52:26 CET 2019

---
+++ model/generated-migrations/PropelMigration_1553791946.php
@@ -0,0 +1,82 @@
+<?php
+
+use Propel\Generator\Manager\MigrationManager;
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1553791946.
+ * Generated on 2019-03-28 17:52:26 by dam2t02
+ */
+class PropelMigration_1553791946
+{
+    public $comment = '';
+
+    public function preUp(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postUp(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL()
+    {
+        return array (
+  'default' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `user`
+
+  CHANGE `deleted` `is_deleted` DATETIME;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL()
+    {
+        return array (
+  'default' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `user`
+
+  CHANGE `is_deleted` `deleted` DATETIME;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+}
