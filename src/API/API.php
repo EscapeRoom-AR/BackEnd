581afd20f3403435d843d113c74aae4369b42a30
alex <5alexbouoliete@gmail.com>
2019-03-31 19:39:41 +0200

    debug

--- src/API/API.php
+++ src/API/API.php
@@ -69,7 +69,7 @@ class API extends \Slim\App {
 	public static function deleteUser(Request $request, Response $response, array $args) {
 		$token = $request->getParsedBody()['token'];
 		$user = \API\API::checkAuthentication($token);
-		$response->getBody()->write($user);
+		$response->getBody()->write($user->getCode());
 		return $response;
 	}
 
