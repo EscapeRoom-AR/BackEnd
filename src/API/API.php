670b6c7f390a2c05e1820c7bb2b4e9b91139e52c
alex <5alexbouoliete@gmail.com>
2019-03-31 20:07:40 +0200

    debug

--- src/API/API.php
+++ src/API/API.php
@@ -70,7 +70,7 @@ class API extends \Slim\App {
 		$paramMap = $request->getParsedBody();
 		$token = $paramMap['token'];
 		$user = \API\API::checkAuthentication($token);
-		$response->getBody()->write(json_encode($user));
+		$response->getBody()->write($user);
 		return $response;
 	}
 
