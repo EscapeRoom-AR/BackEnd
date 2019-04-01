8cf7738d2526b29839de19afab1116591cf7522c
alex <5alexbouoliete@gmail.com>
2019-04-01 22:29:43 +0200

    debug

--- src/API/API.php
+++ src/API/API.php
@@ -66,7 +66,7 @@ class API extends \Slim\App {
 	public static function register(Request $request, Response $response, array $args) {
 		$paramMap = $request->getParsedBody();
 		if ($paramMap['email'] == null || $paramMap['username'] == null || !$paramMap['password'] == null) {
-			return Api::getErrorResp($response, "Email, username, or password were not provided.");
+			return Api::getErrorResp($response, "Email, username, or password were not provided: ".$paramMap['email'].",".$paramMap['username'].",".$paramMap['password']);
 		}
 		$user = \API\Model\UserQuery::create()->filterByUsername($paramMap['username'])->find()->getFirst();
 		if ($user) {
