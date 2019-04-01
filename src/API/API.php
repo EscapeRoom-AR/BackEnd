7c98a1cbb5b45d579c531983be5a3e506e2a7565
alex <5alexbouoliete@gmail.com>
2019-04-01 11:25:15 +0200

    debug

--- src/API/API.php
+++ src/API/API.php
@@ -143,10 +143,12 @@ class API extends \Slim\App {
 		} 
 		$room = $room->toArray();
 		$items = \API\Model\ItemQuery::create()->filterByRoomCode($args['code'])->find()->toArray();
+		$hints = [];
 		foreach ($items as $item) {
-			$item['hints'] = \API\Model\HintQuery::create()->filterByItemCode($item['Code'])->find()->toArray();
+			$hints[$item['Code']] = \API\Model\HintQuery::create()->filterByItemCode($item['Code'])->find()->toArray();
 		}
 		$room['items'] = $items;
+		$room['hints'] = $hints;
 		return $response->withJson($room);
 	}
 	
