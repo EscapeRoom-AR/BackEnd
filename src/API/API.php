df7ac51d10eca616030731b2e47b93f0a72e3ab4
alex <5alexbouoliete@gmail.com>
2019-04-01 18:45:25 +0200

    debug

--- src/API/API.php
+++ src/API/API.php
@@ -138,14 +138,14 @@ class API extends \Slim\App {
 	// Generates a token from a User object.
 	public static function generateToken(User $user) {
 		$header= base64_encode(json_encode(array('alg'=> 'HS256', 'typ'=> 'JWT')) );
-		$payload= base64_encode(json_encode($user->toArray()));
+		$payload= base64_encode($user->getCode());
 		$signature= base64_encode(hash_hmac('sha256', $header. '.'. $payload, Api::$secret_key, true));
 		$jwt_token= $header. '.'. $payload. '.'. $signature;
 		return $jwt_token;
 	}
 
 	// Returns false if token is incorrect, a User object otherwise.
-	public static function auth($token){
+	public static function auth($token) {
 		if (!isset($token) || $token == "") { 
 			return false; 
 		}
@@ -154,9 +154,10 @@ class API extends \Slim\App {
 		if ($jwt_values[2] != $signature) { 
 			return false; 
 		}
-		$user = new User();
+		/*$user = new User();
 		$user->fromArray(json_decode(base64_decode($jwt_values[1]),true));
-		$user = \API\Model\UserQuery::create()->findPK($user->getCode());
+		$user = \API\Model\UserQuery::create()->findPK($user->getCode()); */
+		$user = \API\Model\UserQuery::create()->findPK(base64_decode($jwt_values[1]));
 		if ($user->getDeletedat() != null) { 
 			return false; 
 		}
