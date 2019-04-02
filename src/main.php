57f512d7f2f963bebe4b1914fd164cebd264ee09
alex <5alexbouoliete@gmail.com>
2019-04-02 13:17:13 +0200

    new organisation

--- src/main.php
+++ src/main.php
@@ -12,12 +12,6 @@ require_once('../vendor/autoload.php');
 # Propel configuration
 require_once('../model/generated-conf/config.php');
 
-# Autoload controllers
-//require_once('controller/autoload.php');
-
-# Load token class
-//require_once('utils/token.php');
-
 # Create instance of slim app (configured to show errors)
 $app = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
 
