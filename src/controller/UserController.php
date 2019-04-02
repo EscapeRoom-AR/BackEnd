bfb7562d19d862eabebe89d0acfb562585c49c62
alex <5alexbouoliete@gmail.com>
2019-04-02 14:10:49 +0200

    new organisation

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -13,8 +13,8 @@ class UserController extends Controller {
 
 	public function getUser(Request $request, Response $response, array $args) {
 		//$user = Token::auth($request->getQueryParams()['token']);
-		$user = Token::auth($request->getHeader('Authorization'));
-		return $response->withJson(["code" => 1, "message" => "Ok", "data" => ["token" => $request->getHeader('Authorization')]], 200);
+		$user = Token::auth($request->getHeader('Authorization')[0]);
+		//return $response->withJson(["code" => 1, "message" => "Ok", "data" => ["token" => $request->getHeader('Authorization')]], 200);
 		if (!$user) { 
 			return $this->getErrorTokenResp($response); 
 		}
