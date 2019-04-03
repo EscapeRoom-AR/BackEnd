da54272dec3ad5c00ad00fa3b291ad7546e2bd94
Alex <acuevasdev@gmail.com>
2019-04-03 15:37:09 +0200

    updateUser endpoint route

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -8,12 +8,13 @@ $app->get('/user',			'\Controller\UserController:getUser');
 
 $app->delete('/user',		'\Controller\UserController:deleteUser');
 
+$app->put('/user',			'\Controller\UserController:updateUser');
+
 $app->get('/rooms',			'\Controller\RoomController:getRooms');
 
 $app->get('/room/{code}',	'\Controller\RoomController:getRoom');
 
-/*$app->put('/user',			'\API\API::updateUser');
-
+/*
 $app->post('/game',			'API\API::createRoom');
 
 
