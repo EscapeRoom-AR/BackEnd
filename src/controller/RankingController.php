d30b047ad24e6c34247b997ebb7df32260e5a201
Alex <alexcuevitas@gmail.com>
2019-04-13 18:06:00 +0200

    sort

--- src/controller/RankingController.php
+++ src/controller/RankingController.php
@@ -30,10 +30,11 @@ class RankingController extends Controller
                 return $g1->getTime() < $g2->getTime();
             });
         }
-        for ($i = 0; $i < count($games); $i++) {
-            echo $games[$i];
-            echo $gamesArray[$i];
-        }
+            echo "games";
+            echo var_dump($games);
+            echo "gamesarray";
+            echo var_dump($gamesArray);
+
         return $this->getOkResp($response, $games->toArray());
     }
 
